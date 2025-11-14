ec3eed19a2ef1c9c74ec94e69cae8d07abaea369
mjsadmin <eisuke_koresawa@mjs.co.jp>
2025-11-15 07:55:47 +0900

    Edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   allowedDevOrigins: [
-    "http://tvsgnxpsv:3000",
-    "http://localhost:3000",
-    "http://192.168.9.82:3000",
-    "http://192.168.9.110:3000",
-  ]
+    "localhost",        // ローカル
+    "192.168.9.82",     // サーバーのIP
+    "192.168.9.110",    // 必要なら他の端末
+    "tvsgnxpsv",        // ← 実際にアクセスしているホスト名
+  ],
 };
 
 export default nextConfig;
