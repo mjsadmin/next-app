6c642b8312c9f5af7a792ba41b153cc79863fbea
ekrsw <eisuke.koresawa@gmail.com>
2025-11-15 08:23:37 +0900

    Delete comment from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    "localhost",        // ローカル
-    "192.168.9.82",     // サーバーのIP
-    "192.168.9.110",    // 必要なら他の端末
-    "tvsgnxpsv",        // ← 実際にアクセスしているホスト名
+    "localhost",
+    "192.168.9.82",
+    "192.168.9.110",
+    "tvsgnxpsv",
   ],
 };
 
