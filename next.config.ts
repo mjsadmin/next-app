7d024db3520401d2674220d2ddddfe3c299defb4
mjsadmin <eisuke_koresawa@mjs.co.jp>
2025-11-15 03:03:42 +0900

    test test test

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "http://tvsgnxpsv:3000",
+    "http://localhost:3000",
+    "http://192.168.9.82:3000",
+    "http://192.168.9.110:3000",
+  ]
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      test
+      test test test
     </div>
   );
 }
